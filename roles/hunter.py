b23e9e54345d15e5b2e4ebe105da055ea991a7b8
musuke <ultimatemusuke@gmail.com>
2026-06-06 21:52:32 +0900

    Update hunter.py

--- roles/hunter.py
+++ roles/hunter.py
@@ -1,3 +1,4 @@
 from roles.base import BaseRole
 class Hunter(BaseRole):
-    name, team, has_night_action = "狩人", "村人", True
+    name, team = "狩人", "村人"
+    def get_action_label(self): return "護衛"
